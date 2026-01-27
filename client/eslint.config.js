0c7ed914565616e45ed0fb78f22374d87436e26c
okaybro <66475772+Chaitanya-Keyal@users.noreply.github.com>
2026-01-27 18:26:59 +0000

    feat: initialize

---
+++ client/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^[A-Z]|^React$|^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+];
